ef9a76948192ee107fbb4e241eb8fab491ab69fc
ganink <christiancaleroerazo@gmail.com>
2018-02-20 19:22:05 +0100

    videotube v1.0

---
+++ js/data.js
@@ -0,0 +1,56 @@
+var data = [{
+        id: 1,
+        v1: 'media/video/mario-rabits.mp4',
+        v2: 'media/video/mario-rabits.webm',
+        title: 'Mario + Rabits',
+        description: 'MARIO + RABITS : KINDOM BATTLE añade un nuevo personaje!',
+        type: 'video/mp4'
+    },
+    {
+        id: 2,
+        v1: 'media/audio/Sleepmakeswaves - Its Dark, Its Cold, Its Winter.mp3',
+        img: 'media/images/sleepmakedwaves.jpg',
+        title: 'Sleep-Maked-Waves: Its Dark, Its Cold, Its Winter',
+        description: 'Sleep-Maked-Waves: Its Dark, Its Cold, Its Winter',
+        type: 'audio/mpeg'
+    },
+    {
+        id: 3,
+        v1: 'media/video/dragon-quest.mp4',
+        v2: 'media/video/dragon-quest.webm',
+        title: 'Dragon Quest Builders',
+        description: 'DRAGON QUEST BUILDERS for Nintendo Switch',
+        type: 'video/mp4'
+    },
+    {
+        id: 4,
+        v1: 'media/audio/Death Grips - Get Got.mp3',
+        img: 'media/images/death-grips.png',
+        title: 'Death Grips: Get Got',
+        description: 'Death Grips: Get Got',
+        type: 'audio/mpeg'
+    }, 
+    {
+        id: 5,
+        v1: 'media/video/dark-souls.mp4',
+        v2: 'media/video/dark-souls.webm',
+        title: 'Dark Souls REMASTERED',
+        description: 'DARK SOULS: REMASTERED for Nintendo Switch',
+        type: 'video/mp4'
+    }, 
+    {
+        id: 6,
+        v1: 'media/audio/Death Grips - I Want It I Need It (Death Heated).mp3',
+        img: 'media/images/death-grips-2.png',
+        title: 'Death Grips: I Want It I Need It (Death Heated)',
+        description: 'Death Grips: I Want It I Need It (Death Heated)',
+        type: 'audio/mpeg'
+    }, 
+    {
+        id: 7,
+        v1: 'media/video/custom.mp4',
+        title: 'Video personalizado',
+        description: 'Video personalizado',
+        type: 'video/mp4'
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,152 @@
+var video;
+var data_;
+var list;
+var sw = true;
+var duration = 0;
+var tb = 10;
+window.onload = function () {
+    data_ = data;
+    loadList();
+    $('#pause').hide();
+    $('#back').hide();
+    $('#next').hide();
+    list = $('.list-card');
+    list.click(function () {
+        $('.list-container').find('.div-is-active').removeClass('div-is-active');
+        let x = parseInt($(this).attr('id'));
+        console.log(x);
+        $(this).addClass('div-is-active');
+        $('.section-controls').find('progress').remove();
+        $(data_).each(function (i, element) {
+            if (x == element.id) {
+                let section = $('.section-body');
+                section.empty();
+                section.append(`<video alt="${element.title}" poster="${element.img}"><source src="${element.v1}" alt="${element.title}" type="${element.type}" id="${element.id}"></video>`);
+                video = $('video');
+                desActive();
+            }
+            if(element.id == '7'){
+                let section = $('.section-body');
+                section.empty();
+                section.append(`<video alt="${element.title}" poster="${element.img}"><source src="${element.v1}" alt="${element.title}" type="${element.type}" id="${element.id}"><track src="media/translate/cc.vtt" srclang="es" label="Español" default></video>`);
+                video = $('video');
+                video[0].volume = 0;
+                desActive();
+            }
+        })
+    })
+    if (sw) {
+        let section = $('.section-body');
+        section.empty();
+        section.append(`<video alt="${data_[0].title}"><source src="${data_[0].v1}" alt="${data_[0].title}" type="video/mp4" id="${data_[0].id}"/></video>`);
+        video = $('video');
+        sw = false;
+        $('.list-container').find(`#1`).addClass('div-is-active');
+        desActive();
+    }
+}
+
+function loadVol() {
+    let value = video[0].volume;
+}
+
+function loadList() {
+    let list = $('.list-container');
+    let c_ = 0;
+    $(data_).each(function (i, element) {
+        let c = 'c' + c_;
+        if (element.type == 'audio/mpeg') {
+            list.append(`<div class="list-card ${c}" id="${element.id}">`);
+            $('.c' + c_).append(`<div tabindex="${tb}" title="${element.title}"><span class="icon-13" title="${element.title}"><h4 role="heading" level="2">${element.description}</h4></span></div>`);
+            c_++;
+            tb++;
+        }
+        if (element.type == 'video/mp4') {
+            list.append(`<div class="list-card ${c}" id="${element.id}">`);
+            $('.c' + c_).append(`<div tabindex="${tb}" title="${element.title}"><span class="icon-12" title="${element.title}"><h4 role="heading" level="2">${element.description}</h4></span></div>`);
+            c_++;
+            tb++;
+        }
+    })
+}
+
+function active() {
+    $('#pause').show('slow');
+    $('#back').show('slow');
+    $('#next').show('slow');
+    $('#play').hide('slow');
+}
+
+function desActive() {
+    $('#pause').hide('slow');
+    $('#back').hide('slow');
+    $('#next').hide('slow');
+    $('#play').show('slow');
+}
+
+$('#play').click(function () {
+    video[0].play();
+    active();
+})
+
+$('#pause').on('click', function () {
+    video[0].pause();
+    desActive();
+})
+
+$('#stop').on('click', function () {
+    video[0].pause();
+    video[0].currentTime = 0;
+    desActive();
+})
+
+$('#back').on('click', function () {
+    video[0].currentTime -= 10;
+})
+
+$('#next').on('click', function () {
+    video[0].currentTime += 10;
+})
+
+$('#up').on('click', function () {
+    video[0].volume += 0.1;
+})
+
+$('#down').on('click', function () {
+    video[0].volume -= 0.1;
+})
+
+$('#mute').on('click', function () {
+    video[0].volume = 0;
+})
+
+$('#backElement').click(function () {
+    $('.list-container').find('.div-is-active').removeClass('div-is-active');
+    let id = parseInt($('video').find('source').attr('id'));
+    id--;
+    $(data_).each(function (i, element) {
+        if (id == element.id) {
+            $('.list-container').find(`#${id}`).addClass('div-is-active');
+            let section = $('.section-body');
+            section.empty();
+            section.append(`<video alt="${element.title}" poster="${element.img}"><source src="${element.v1}" alt="${element.title}" type="${element.type}" id="${element.id}"></video>`);
+            video = $('video');
+            desActive();
+        }
+    })
+})
+$('#nextElement').click(function () {
+    $('.list-container').find('.div-is-active').removeClass('div-is-active');
+    let id = parseInt($('video').find('source').attr('id'));
+    id++;
+    $(data_).each(function (i, element) {
+        if (id == element.id) {
+            $('.list-container').find(`#${id}`).addClass('div-is-active');
+            let section = $('.section-body');
+            section.empty();
+            section.append(`<video alt="${element.title}" poster="${element.img}"><source src="${element.v1}" alt="${element.title}" type="${element.type}" id="${element.id}"></video>`);
+            video = $('video');
+            desActive();
+        }
+    })
+})
